f8d6219628321a98036a0bf10c24752258a8e4c7
jesusiniguez <jesus.iniguezgarcia@alum.uca.es>
2016-06-01 00:13:43 +0200

    Añadiendo Carrito Diabéticos a main.py

--- main.py
+++ main.py
@@ -66,7 +66,20 @@ elif Choice == 2:
 	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 1, driver, "DIA zumo exprimido naranja envase 1 lt")
 
 elif Choice == 3:
-    print('Single')
+	print "---   CARRITO DE COMPRA DE PRODUCTOS PARA DIABÉTICOS   ---"
+    	Total += lgoritmos.Imprimir_y_Anadir(Max, Total, 1, "DIA arroz integral paquete 1 Kg")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 1, "DIA lentejas pardina bolsa 1 Kg")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 1, "DIA menestra de verduras bolsa 1 kg")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 2, "DIA atun claro en aceite de oliva contenido bajo en sal pack 3 latas 156gr")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 2, "DIA aceite de oliva extra virgen botella 750 ml")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 6, "DIA leche semidesnatada envase 1 lt")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 2, "LA RECETA huevos frescos categoria A clase M estuche 12 uds")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 1, "DIA garbanzo extra bolsa 1 KG")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 12, "DAURA cerveza SIN GLUTEN botella 33 cl")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 2, "DIA postre soja chocolate pack 4 unidades 100 gr")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 3, "DIA agua natural 1.5l")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 2, "DR. SCHAR baguette SIN GLUTEN envase 350 gr")
+	Total += algoritmos.Imprimir_y_Anadir(Max, Total, 1, "GALLO pluma SIN GLUTEN paquete 500 gr")
 else:
     print('Error de inserción')
 
