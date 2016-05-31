62f6f910ac4a296012e4b31eeccdbe30f89bfa28
jesusiniguez <jesus.iniguezgarcia@alum.uca.es>
2016-05-31 17:33:31 +0200

    Renombrando obtencion a algoritmos y añadiendo comentarios

--- algoritmos.py
+++ algoritmos.py
@@ -9,17 +9,6 @@ from selenium.common.exceptions import NoAlertPresentException
 import requests
 import string
 
-""" 
-Usamos: 
-	xpath a partir de Firebug
-	Selenium para obtener las url de las busquedas
-	PhantomJS para no abrir un navegador con selenium
-	Requests para obtener el HTML de la url
-	LXML para transformar a string un HTML
-	string para limpiar los textos de €
-"""
-
-
 #Funcion auxiliar para convertir un string de "euros" obtenido de la web 
 #que devuelva un número flotante que corresponda al precio del string
 def Precio_a_Numero(Dato):
@@ -32,12 +21,18 @@ def Precio_a_Numero(Dato):
 	return float(Precio_Descompuesto)/100
 
 
+
+#Funcion de búsqueda que utiliza selenium para 
+#acceder a la web concreta de un artículo dado un filtro
 def Busqueda(driver, filtro):
 	driver.get("http://www.dia.es/compra-online/")
 	driver.find_element_by_xpath("//input[@id='search']").send_keys(filtro)
 	driver.find_element_by_xpath("//input[@type='image']").click()
 	driver.find_element_by_xpath(u"//div[@id='content']/div[3]/div/div[2]/div[5]/div/div/a/span/img") .click()
 
+
+#Funcion que, dado un filtro, busca la 
+#URL del artículo e imprime su precio y descripción, además de devolver el precio
 def Imprimir_y_Anadir(driver, filtro):
 	Busqueda(driver, filtro)
 	url = requests.get(driver.current_url)
